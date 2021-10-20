e10c2c75c510bfacb90fb3560c9bb17ad1b53597
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-10-20 16:14:42 -0500

    using sage_data_client and fixed bug when vsn is na

--- main.py
+++ main.py
@@ -4,39 +4,28 @@ from urllib.request import urlopen
 import streamlit as st
 from datetime import datetime
 from hashlib import sha1
-
-
-series_name = "upload"
-
-
-def do_query(query):
-    data = json.dumps(query).encode()
-    with urlopen("https://data.sagecontinuum.org/api/v1/query", data) as f:
-        df = pd.read_json(f, lines=True)
-    if len(df) == 0:
-        return df
-    # join metacolumns
-    df = df.join(pd.json_normalize(df["meta"]))
-    return df
+import sage_data_client
 
 
 @st.cache(ttl=3600)
 def get_nodes_last_30_days():
-    return sorted(set(do_query({
-        "start": "-30d",
-        "tail": 1,
-        "filter": {
-            "name": series_name,
+    df = sage_data_client.query(
+        start="-30d",
+        tail=1,
+        filter={
+            "name": "upload",
             "vsn": ".*",
         }
-    }).vsn))
+    )
+    return sorted(set(df["meta.vsn"].dropna()))
 
 
-st.sidebar.title("Image Viewer")
+st.sidebar.title("Upload Viewer")
 node = st.sidebar.selectbox("Node", get_nodes_last_30_days())
 date = st.sidebar.date_input("Date")
 time = st.sidebar.time_input("Time")
 window = pd.to_timedelta(st.sidebar.text_input("Time Window", "1h"))
+media_type = st.sidebar.selectbox("Media Type", ["Image", "Audio"])
 st.sidebar.text("Datetime is in UTC!")
 st.sidebar.text("05:00 UTC = midnight CST")
 st.sidebar.text("17:00 UTC = noon CST")
@@ -49,7 +38,7 @@ query = {
     "start": start.strftime("%Y-%m-%dT%H:%M:%SZ"),
     "end": end.strftime("%Y-%m-%dT%H:%M:%SZ"),
     "filter": {
-        "name": series_name,
+        "name": "upload",
         "vsn": node,
     }
 }
@@ -57,29 +46,48 @@ query = {
 st.title("Results")
 st.code(f"curl -s -H 'Content-Type: application/json' https://data.sagecontinuum.org/api/v1/query -d \'{json.dumps(query)}\'")
 
-df = do_query(query)
+df = sage_data_client.query(**query)
 if len(df) == 0:
     st.warning(f"No query results found!")
     st.stop()
 
-images = df[df.value.str.endswith(".jpg") | df.value.str.endswith(".png")]
-if len(images) == 0:
-    st.warning(f"No images found!")
-    st.stop()
+if media_type == "Audio":
+    audios = df[df.value.str.endswith(".flac") | df.value.str.endswith(".mp3") | df.value.str.endswith(".wav")]
 
-download_file = "\n".join(images.value)
-download_file += "\n"
-filename = "urls-" + sha1(download_file.encode()).hexdigest()[:8] + ".txt"
-st.sidebar.download_button("Download List of URLs", download_file, filename)
-st.sidebar.code(f"# download all urls with wget\nwget -r -N -i {filename}")
+    if len(audios) == 0:
+        st.warning(f"No audio found!")
+        st.stop()
 
-for plugin, df_plugin in images.groupby("plugin"):
-    st.title(plugin)
+    st.title("Audio Samples")
 
-    urls = list(df_plugin.value)
-    captions = list(df_plugin.timestamp)
-
+    urls = list(audios.value)
     stride = len(urls)//16 + 1
     if stride > 1:
         st.warning(f"Too many images ({len(urls)}). Showing every {stride} images.")
-    st.image(urls[::stride], caption=captions[::stride], width=640)
+    for url in urls[::stride]:
+        st.audio(url)
+        st.caption(url)
+elif media_type == "Image":
+    images = df[df.value.str.endswith(".jpg") | df.value.str.endswith(".png")]
+
+    if len(images) == 0:
+        st.warning(f"No images found!")
+        st.stop()
+
+    # add download file and button
+    download_file = "\n".join(images.value)
+    download_file += "\n"
+    filename = "urls-" + sha1(download_file.encode()).hexdigest()[:8] + ".txt"
+    st.sidebar.download_button("Download Image List", download_file, filename)
+    st.sidebar.code(f"# download all urls with wget\nwget -r -N -i {filename}")
+
+    for plugin, df_plugin in images.groupby("meta.plugin"):
+        st.title(plugin)
+
+        urls = list(df_plugin.value)
+        captions = list(df_plugin.timestamp)
+
+        stride = len(urls)//16 + 1
+        if stride > 1:
+            st.warning(f"Too many images ({len(urls)}). Showing every {stride} images.")
+        st.image(urls[::stride], caption=captions[::stride], width=640)
